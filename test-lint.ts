14e32a3afa5e45173dd4d897fc3e0fc6d1bdc19a
Ryan McHenry <ryanmmchenry@gmail.com>
2025-09-06 10:10:05 -0500

    test: verify auto-fix

---
+++ test-lint.ts
@@ -0,0 +1,8 @@
+// Test file with formatting and linting issues
+const unused_variable = "test";
+let another = "value";
+
+function testFunction() {
+  console.log("test");
+  var outdated = "using var";
+}
