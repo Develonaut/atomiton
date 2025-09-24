f4570767c2d06ce5d8f780dc50b99cd25b6b7db1
Ryan McHenry <ryanmmchenry@gmail.com>
2025-09-24 16:57:25 -0500

    fix: restore accidentally deleted globalErrorHandler util

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,5 +1,5 @@
 import App from "#App";
-import { setupGlobalErrorHandlers } from "#utils/globalErrorHandler";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 // Import UI theme variables first - must be before ReactFlow styles
@@ -8,9 +8,6 @@ import "@atomiton/ui/theme/variables.css";
 import "#index.css";
 // Import editor styles for ReactFlow nodes - after theme variables
 import "@atomiton/editor/index.css";
-
-// Set up global error handlers before rendering
-setupGlobalErrorHandlers();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
